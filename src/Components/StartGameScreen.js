a11d887e4449a5945d31d73ccd34246bf6254f58
gandhisamay <gandhisamay200@gmail.com>
2021-08-12 15:44:27 +0530

    player names added to state

--- src/Components/StartGameScreen.js
+++ src/Components/StartGameScreen.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import UserInput from './UserInput';
 import '../Styles/App.css';
-import Button from './Button';
 
 const StartGameScreen = () => {
     return (
         <div className="startGameScreen">
             <h1>Connect 4</h1>
-            <div className="startGame">
-                <div className="playerNames">
-                    <UserInput message="Player 1" placeholder="Enter your name" />
-                    <UserInput message="Player 2" placeholder="Enter your name" />
-                </div>
-                <Button message="Start Game!" />
-            </div>
+            <UserInput 
+                message1="Player 1" 
+                message2="Player 2" 
+                placeholder1="Enter your name" 
+                placeholder2="Enter your name" 
+                buttonMessage="Start Game!" 
+            />
         </div>
     );
 }
--- src/Components/UserInput.js
+++ src/Components/UserInput.js
@@ -1,13 +1,41 @@
-import React from 'react';
-import '../Styles/App.css';
+import React from "react";
+import "../Styles/App.css";
+import { connect } from 'react-redux';
+import { startGame } from '../actions';
 
-const UserInput = ({message, placeholder})=>{
-    return (
-    <div className="userInput">
-       <label>{message}</label> 
-       <input type="text" maxLength="10" placeholder={placeholder}/>
-    </div>
-    );
+class UserInput extends React.Component {
+    state = { player1: '', player2: '' };
+
+    onFormSubmit = (e) =>{
+        e.preventDefault(); 
+        startGame(this.state.player1, this.state.player2); 
+        console.log(this.state.player1);
+    }
+
+    on
+    render() {
+        return (
+            <form onSubmit={this.onFormSubmit}>
+                <div className="userInput">
+                    <label>{this.props.message1}</label>
+                    <input type="text" maxLength="10" placeholder={this.props.placeholder1} onChange={(e) => { this.setState({ player1: e.target.value }) }} />
+                </div>
+                <div className="userInput">
+                    <label>{this.props.message2}</label>
+                    <input type="text" maxLength="10" placeholder={this.props.placeholder2} onChange={(e) => { this.setState({ player2: e.target.value }) }} />
+                </div>
+                <div className="button">
+                    <button type="submit">{this.props.buttonMessage}
+                    </button>
+                </div>
+            </form>
+        );
+    }
+};
+
+const mapStateToProps = (state) => {
+    console.log(state);
+    return {};
 }
 
-export default UserInput;
+export default connect(mapStateToProps, { startGame })(UserInput);
