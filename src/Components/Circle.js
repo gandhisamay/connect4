cb20f806b757f5d44641db15bee0d35c1b3ba379
gandhisamay <gandhisamay200@gmail.com>
2021-08-18 15:24:27 +0530

    stuck at repeating the circles

---
+++ src/Components/Circle.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Circle extends React.Component{
+    render(){
+        return (
+            <div className="circle">
+
+            </div>
+        );
+    }
+}
+
+export default Circle;
--- src/Components/GameScreen.js
+++ src/Components/GameScreen.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import {connect } from 'react-redux';
+import '../Styles/App.css'
+// import Circle from './Circle';
 
 class GameScreen extends React.Component{
+
+    
+
     render(){
-        return <div>Game Screen</div>
+        console.log("props",this.props)
+        return (
+        <div className="gameScreen">
+            <div className="showPlayerName">
+                {/* <h1>{this.props.players[0]}</h1> */}
+                <h1>Samay</h1>
+                <h1>Ansh</h1>
+            </div>
+            <div className="game">
+            </div>
+        </div>
+        );
     }
 }
 
